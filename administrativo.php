aafa726abc76d593c97a4548776c919d89346244
Rennanmax80 <48867131+Rennanmax80@users.noreply.github.com>
2019-05-24 20:54:42 -0300

    Add files via upload

---
+++ administrativo.php
@@ -0,0 +1,12 @@
+<?php
+	session_start();
+    if(!empty($_SESSION['usuarioId'])){
+		echo "Bem vindo: ". $_SESSION['usuarioNome']. "<br>";
+		echo "<a href='sair.php'>Sair</a>";
+	}else{
+		$_SESSION['logindeslogado'] = "Área restrita! <br> Logue com seu usuário.";
+		header("Location: index.php");
+	}
+		
+?>
+
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="Cesar Szpak">
+    <link rel="icon" href="">
+    <title>Rennan - Login</title>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="css/signin.css" rel="stylesheet">
+    <script src="js/ie-emulation-modes-warning.js"></script>
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" method="POST" action="valida.php">
+        <h2 class="form-signin-heading">Tela de login</h2>
+        <label for="inputEmail" class="sr-only">Email</label>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+        <label for="inputPassword" class="sr-only">Senha</label>
+        <input type="password" name="senha" id="inputPassword" class="form-control" placeholder="Senha" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Logar</button>
+		    <div class="checkbox mb-3">
+              <label>
+               <input type="checkbox" value="remember-me"> Lembrar-me
+              </label>
+            </div>
+      </form>
+	  <p class="text-center text-danger">
+			<?php if(isset($_SESSION['loginErro'])){
+				echo $_SESSION['loginErro'];
+				unset($_SESSION['loginErro']);
+			}?>
+		</p>
+		<p class="text-center text-success">
+			<?php 
+			if(isset($_SESSION['logindeslogado'])){
+				echo $_SESSION['logindeslogado'];
+				unset($_SESSION['logindeslogado']);
+			}
+			?>
+		</p>
+    </div>
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
